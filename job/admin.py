65c8317c2e281ddc9cf03f8acbe7668925ad5ada
Ali Parvizi <snparvizi75@gmailc.om>
2021-08-21 18:38:47 +0430

    admin register

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'parent']
+    list_filter = ['parent']
+    search_fields = ['title', 'parent__title']
+
+
+admin.site.register(Job, JobAdmin)
